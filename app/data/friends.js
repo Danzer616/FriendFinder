0bff560969718c8ad37e7b10edc2268435735f82
Danzer <dan.alexander616@gmail.com>
2019-03-22 22:28:48 -0600

    Added friend js file

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+
+// ==========================================
+//This file stores the object that will keep all of the friends, similar to a sql database
+// ==========================================
+
+var friendObject = [
+    {name: "Rick Deckard",
+    photo: "https://i2.wp.com/www.everythingaction.com/wp-content/uploads/2012/08/rick-deckard.jpg",
+    scores: [
+        4,
+        3,
+        2,
+        1,
+        5,
+        2,
+        4,
+        4,
+        3,
+        1
+    ]
+    },
+    {name: "Rachael",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi6pbzokpXhAhXs6oMKHdXwDBkQjRx6BAgBEAU&url=https%3A%2F%2Fwww.spotern.com%2Fen%2Fwanted%2Fmovie%2Fblade-runner%2F32790%2Fthe-cigarette-of-rachel-sean-young-in-blade-runner&psig=AOvVaw1khcCHjpe_l9LKlrPv29cp&ust=1553323407995030",
+    scores: [
+        1,
+        2,
+        3,
+        2,
+        1,
+        1,
+        2,
+        2,
+        3,
+        1
+    ]
+    },
+    {name: "Eldon Tyrell",
+    photo: "https://www.leemoyer.com/portraitsfull/h40EE2895#h40ee2895",
+    scores: [
+        5,
+        4,
+        4,
+        4,
+        5,
+        4,
+        4,
+        5,
+        3,
+        4
+    ]
+    },
+    {name: "Roy Batty",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiNltX_tZfhAhUr5oMKHa8dAvgQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Froy_batty_reply&psig=AOvVaw07ZtLdmKHd92wKEN19Asl3&ust=1553401366080030",
+    scores: [
+        2,
+        2,
+        1,
+        1,
+        1,
+        2,
+        1,
+        1,
+        1,
+        4
+    ]
+    }
+]
+
+// ==========================================
+//This exports the object so it can be used in other files
+// ==========================================
+module.exports = friendObject
